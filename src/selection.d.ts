83446887cc1f51bfcf7919e571a7eb5b4ff73144
Percy Chuzon <percyrondan1995@gmail.com>
2026-08-02 17:48:12 -0500

    se agrego tipado para ts

---
+++ src/selection.d.ts
@@ -0,0 +1,5 @@
+// dist/selection.d.ts
+declare module "stellarstyles.css/selection" {
+  const css: string;
+  export default css;
+}
---
+++ src/stellarstyles.d.ts
@@ -0,0 +1,5 @@
+// dist/stellarstyles.d.ts
+declare module "stellarstyles.css" {
+  const css: string;
+  export default css;
+}
